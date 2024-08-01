c711cb8e7d8f5396153bd570ff358be7d22bfefa
dipalirahane <soulsoft.soul120@gmail.com>
2024-08-01 11:19:57 +0530

    AUTHERIZATION

---
+++ app/Filters/JWTAuthFilter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTAuthFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        $authHeader = $request->getHeaderLine('Authorization');
+        if (!$authHeader) {
+            return service('response')
+                ->setJSON(['message' => 'Unauthorized: No token provided'])
+                ->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED);
+        }
+
+        $token = str_replace('Bearer ', '', $authHeader);
+
+        try {
+            $decoded = JWT::decode($token, new Key(getenv('JWT_SECRET_KEY'), 'HS256'));
+            // Optionally store user info in session or request object
+        } catch (\Exception $e) {
+            return service('response')
+                ->setJSON(['message' => 'Unauthorized: Invalid or expired token'])
+                ->setStatusCode(ResponseInterface::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do nothing after
+    }
+}

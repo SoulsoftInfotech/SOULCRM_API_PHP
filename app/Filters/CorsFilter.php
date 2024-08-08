41f8eb40f8e90076e717e1c94a091ba274892cbf
dipalirahane <soulsoft.soul120@gmail.com>
2024-08-08 11:57:57 +0530

    cors filter

--- app/Filters/CorsFilter.php
+++ app/Filters/CorsFilter.php
@@ -16,14 +16,14 @@ class CorsFilter implements FilterInterface
         header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
         // Handle preflight OPTIONS request
-        if ($request->getMethod(true) === 'OPTIONS') {
+        // if ($request->getMethod(true) === 'OPTIONS') {
             $response = service('response');
             $response->setStatusCode(200);
             $response->setHeader('Access-Control-Allow-Origin', '*');
             $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
             return $response;
-        }
+        // }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)

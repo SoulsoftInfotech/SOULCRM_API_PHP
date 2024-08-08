94e27ce0d4ba5bfee882939cfa046348d5aa9cfd
dipalirahane <soulsoft.soul120@gmail.com>
2024-08-08 14:18:55 +0530

    CORS FILTER

--- app/Filters/CorsFilter.php
+++ app/Filters/CorsFilter.php
@@ -10,20 +10,20 @@ class CorsFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        // // Allow all origins, methods, and headers
-        // header("Access-Control-Allow-Origin: *");
-        // header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-        // header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        // Allow all origins, methods, and headers
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-        // // Handle preflight requests
-        // if ($request->getMethod(true) === 'OPTIONS') {
-        //     $response = service('response');
-        //     $response->setStatusCode(200);
-        //     $response->setHeader('Access-Control-Allow-Origin', '*');
-        //     $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        //     $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        //     return $response;
-        // }
+        // Handle preflight requests
+        if ($request->getMethod(true) === 'OPTIONS') {
+            $response = service('response');
+            $response->setStatusCode(200);
+            $response->setHeader('Access-Control-Allow-Origin', '*');
+            $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            return $response;
+        }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)

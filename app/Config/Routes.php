da5a373319d706ed2fc50a26d664640512931c0b
dipalirahane <soulsoft.soul120@gmail.com>
2024-08-28 09:44:28 +0530

    post

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,33 +32,33 @@ $routes->group("api/leads", ['filter' => 'authFilter'],function($routes){
      
     //-----------------------------create ,update,upload-----------------------//
     $routes->post('save','Api\Lead\CreateLeadController::create');
-    $routes->get('leadsandcust','Api\Lead\CreateLeadController::getAllLeadswithCustomer');
-    $routes->get('leads','Api\Lead\CreateLeadController::getAllLeads');
-    $routes->get('lead/(:num)','Api\Lead\CreateLeadController::getLeadById/$1');
+    $routes->post('leadsandcust','Api\Lead\CreateLeadController::getAllLeadswithCustomer');
+    $routes->post('leads','Api\Lead\CreateLeadController::getAllLeads');
+    $routes->post('lead/(:num)','Api\Lead\CreateLeadController::getLeadById/$1');
     $routes->post('update/(:num)','Api\Lead\CreateLeadController::update/$1');
     $routes->post('upload','Api\Lead\CreateLeadController::itemExcelUpload');
-    $routes->get('customers','Api\Lead\CreateLeadController::getAllCustomers');
+    $routes->post('customers','Api\Lead\CreateLeadController::getAllCustomers');
     $routes->post('customer/(:num)','Api\Lead\CreateLeadController::updateWithCustomer/$1');
-    $routes->get('getleadtype','Api\Lead\CreateLeadController::leadOptions');
+    $routes->post('getleadtype','Api\Lead\CreateLeadController::leadOptions');
 
 
     //--------------------------get data of all types of booking -------------//
-    $routes->get('booking','Api\Lead\CreateLeadController::getAllBookingDone');
-    $routes->get('customer','Api\Lead\CreateLeadController::getAllCustomer');
-    $routes->get('dataentry','Api\Lead\CreateLeadController::getAllDataEntry');
-    $routes->get('visitrequired','Api\Lead\CreateLeadController::getAllVisitRequired');
-    $routes->get('installations','Api\Lead\CreateLeadController::getAllInstallation');
-    $routes->get('potentials','Api\Lead\CreateLeadController::getAllPotential');
-    $routes->get('demo','Api\Lead\CreateLeadController::getAllDemo');
-    $routes->get('callback','Api\Lead\CreateLeadController::getAllCallBack');
-    $routes->get('notinterested','Api\Lead\CreateLeadController::getAllNotInterested');
+    $routes->post('booking','Api\Lead\CreateLeadController::getAllBookingDone');
+    $routes->post('customer','Api\Lead\CreateLeadController::getAllCustomer');
+    $routes->post('dataentry','Api\Lead\CreateLeadController::getAllDataEntry');
+    $routes->post('visitrequired','Api\Lead\CreateLeadController::getAllVisitRequired');
+    $routes->post('installations','Api\Lead\CreateLeadController::getAllInstallation');
+    $routes->post('potentials','Api\Lead\CreateLeadController::getAllPotential');
+    $routes->post('demo','Api\Lead\CreateLeadController::getAllDemo');
+    $routes->post('callback','Api\Lead\CreateLeadController::getAllCallBack');
+    $routes->post('notinterested','Api\Lead\CreateLeadController::getAllNotInterested');
 
 
     // -------------------------------count of lead types------------------------//
-    $routes->get('countlead','Api\Lead\CreateLeadController::countLeadstype');
-    $routes->get('countpotential','Api\Lead\CreateLeadController::countPotentialtype');
-    $routes->get('countinstallation','Api\Lead\CreateLeadController::countInstallationtype');
-    $routes->get('countbookingdone','Api\Lead\CreateLeadController::countBookingDonetype');
+    $routes->post('countlead','Api\Lead\CreateLeadController::countLeadstype');
+    $routes->post('countpotential','Api\Lead\CreateLeadController::countPotentialtype');
+    $routes->post('countinstallation','Api\Lead\CreateLeadController::countInstallationtype');
+    $routes->post('countbookingdone','Api\Lead\CreateLeadController::countBookingDonetype');
 
     //---------------------------------count of all leads types---------------------//
     $routes->post('countofall','Api\Lead\CreateLeadController::countAllTypes');
